b6c8e1cdc6f5d410ff9a0cc0f0ada124d1417951
kpuchacz <71168561+kpuchacz@users.noreply.github.com>
2021-01-31 10:13:24 +0100

    feat(app): AIO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './component/home/home.component';
+import {PostsListComponent} from './component/posts-list/posts-list.component';
+import {AboutComponent} from './component/about/about.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsListComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+export const routingComponents = [
+  HomeComponent,
+  AboutComponent,
+  PostsListComponent,
+  PageNotFoundComponent
+];
+
+
